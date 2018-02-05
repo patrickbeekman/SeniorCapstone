0012be7f7a70a35abca480c320348c8d5f4cf439
patrickbeekman <patrickbeekman1095@gmail.com>
2018-02-05 10:54:03 -0500

    Installing nodejs, npm, and associated packages on my laptop. Updated to the use of sendFile for package routing. Moved the index file to the views folder. Edited the routes.js file to match this.

--- app/routes.js
+++ app/routes.js
@@ -29,7 +29,7 @@ var Nerd = require('./models/nerd');
         // frontend routes =========================================================
         // route to handle all angular requests
         app.get('*', function(req, res) {
-            res.sendfile('./public/views/index.html'); // load our public/index.html file
+            res.sendFile('/home/patt/Documents/senior_year/SeniorCapstone/public/views/index.html'); // load our public/index.html file
         });
 
     };
