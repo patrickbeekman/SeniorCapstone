c85988e137ff23bcf718e931511fb1316edfb2e3
patrickbeekman <patrickbeekman1095@gmail.com>
2018-02-19 21:48:23 -0500

    Method created to add path to a filename. Edited some filenames and inputs to methods.

--- src/tone_analyzer.py
+++ src/tone_analyzer.py
@@ -30,18 +30,17 @@ class MyToneAnalyzer():
                 exit(0)
         return tone_resp
 
-    def strip_text_from_json(self, filename, newfilename=""):
+    def strip_text_from_json(self, filename, newfilename="hundred_tweets.json"):
         df = pd.read_json(filename)
         tweet_text = df['text']
         hundred_tweets = ""
         for tweet in tweet_text:
-            s_tweet = tweet.strip().replace('.','')
+            s_tweet = tweet.strip().replace('.', '')
             s_tweet += '..'
             hundred_tweets += " " + s_tweet
-        print(hundred_tweets)
         d = {'text': [hundred_tweets]}
         new_df = pd.DataFrame(data=d).to_json(orient='records')[1:-1]
-        with open('file_name.json', 'w') as f:
+        with open(newfilename, 'w') as f:
             f.write(new_df)
 
     def dump_json_to_file(self, data, filename):
@@ -52,18 +51,19 @@ class MyToneAnalyzer():
         with open(filename, 'w') as out:
             json.dump(data, out)
 
+    def path_name(self, filename):
+        return os.path.dirname(__file__) + filename
+
 def main():
     ta = MyToneAnalyzer()
     analyzer = ta.create_connection('2018-02-07')
 
-    filename_tone = os.path.dirname(__file__) + "/../test_text.json"
-    tone_resp = ta.analyze_json_file(analyzer, filename_tone)
-    #print(json.dumps(tone_resp, indent=2))
+    tone_resp = ta.analyze_json_file(analyzer, ta.path_name("/../test_text.json"))
+    #print(json.dumps(tone_resp, indent=2, separators=(',', ': ')))
 
-    filename_tweet = os.path.dirname(__file__) + "/../small_tweets.json"
-    ta.strip_text_from_json(filename_tweet)
-    json.dumps(ta.analyze_json_file(analyzer, os.path.dirname(__file__) + "/file_name.json"))
-    ta.dump_json_to_file(json.dumps(ta.analyze_json_file(analyzer, os.path.dirname(__file__) + "/file_name.json")), "hi.json")
+    ta.strip_text_from_json(ta.path_name("/../small_tweets.json"), "/hundred_tweets.json")
+    tweet_resp = ta.analyze_json_file(analyzer, ta.path_name("/hundred_tweets.json"))
+    ta.dump_json_to_file(json.dumps(tweet_resp, indent=4, separators=(',', ': ')), "/tweets_tone.json")
 
 if __name__ == "__main__":
     main()
