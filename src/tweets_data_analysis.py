d1ffb7f5731263731e136fd4909291e38e5cfc61
patrickbeekman <patrickbeekman1095@gmail.com>
2018-02-25 14:22:07 -0500

    merged the analysis and tweets together into one file that can be read into a pandas dataframe.

--- src/tweets_data_analysis.py
+++ src/tweets_data_analysis.py
@@ -1,44 +1,37 @@
 import pandas as pd
 import os
-import json
-import sys
+
 
 class TweetsDataAnalysis:
 
-    def hello(self):
-        tone_analysis_path = os.path.dirname(__file__) + "/../data/analysis/"
-        my_tweets_json = os.path.dirname(__file__) + "/../data/tweets.json"
+    def attach_analysis_to_tweet(self):
+        analysis_path = os.path.dirname(__file__) + "/../data/all_analysis.json"
+        tweets_path = os.path.dirname(__file__) + "/../data/tweets.json"
 
-        tone_analysis_json = [pos_json for pos_json in os.listdir(tone_analysis_path) if pos_json.endswith(".json")]
+        analysis = pd.read_json(analysis_path)
+        tweets = pd.read_json(tweets_path)
+        tweets['sentence_id'] = range(0, len(tweets))
 
-        all_data = []
+        merged = pd.merge(left=tweets, right=analysis, left_on='sentence_id', right_on='sentence_id')
+        cols = list(merged.columns)
+        print(cols)
+        print("text_y: ", cols[-2])
+        cols[-2] = 'processed_text'
+        print("text_x: ", cols[-9])
+        cols[-9] = 'text'
+        merged.columns = cols
 
-        for js in tone_analysis_json:
-            with open(os.path.join(tone_analysis_path, js)) as json_file:
-                data = pd.read_json(json_file)
-                all_data.append(data)
-
-        all_data = pd.concat(all_data, axis=1)
-        print(all_data['text'])
-        #df = pd.read_json(os.path.dirname(__file__) + "/../data/analysis/tone_tweet_1.json")
-        #print(df['text'])
-
-
-    def rename_files(self):
-        path = os.path.dirname(__file__) + "/../data/analysis/"
-        l = [(x, "tone_tweet_" + x[11:-5].zfill(4) + ".json") for x in os.listdir(path) if
-             x.startswith("tone_tweet_") and x.endswith(".json")]
-
-        for oldname, newname in l:
-            os.rename(os.path.join(path, oldname), os.path.join(path, newname))
-
-
+        output_file_path = os.path.dirname(__file__) + "/../data/merged_analysis.json"
+        with open(output_file_path, 'w') as file:
+            file.write(merged.to_json(orient='records'))
 
 
 def main():
     tda = TweetsDataAnalysis()
-    #tda.hello()
-    tda.rename_files()
+    tda.attach_analysis_to_tweet()
+
+    data = pd.read_json(os.path.dirname(__file__) + "/../data/merged_analysis.json", orient='records')
+    #print(data['tones']['tone_id'])
 
 if __name__ == "__main__":
     main()
