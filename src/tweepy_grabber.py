9857e37d8147cef888ba9743b736579e514fa86d
patrickbeekman <patrickbeekman1095@gmail.com>
2018-03-12 19:06:11 -0400

    created tests for the tweepy grabber class.

--- src/tweepy_grabber.py
+++ src/tweepy_grabber.py
@@ -9,9 +9,12 @@ class TweepyGrabber:
         api = tweepy.API(auth)
         return api
 
-    def get_users_timeline(self, api, screen_name):
+    def get_users_timeline(self, api, screen_name, output_file_name):
         all_tweets = []
-        new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+        try:
+            new_tweets = api.user_timeline(screen_name=screen_name, count=200)
+        except Exception as e:
+            print("Error accessing usertimeline:", e.message)
         all_tweets.extend(new_tweets)
         # get the tweet id of the last tweet grabbed to know where to start grabbing
         # the next 200 tweets
@@ -24,16 +27,16 @@ class TweepyGrabber:
             oldest = all_tweets[-1].id - 1
             print("Downloaded ", len(all_tweets), " so far =)")
 
-        json_filename = os.path.dirname(__file__) + "/../data/tweets.json"
+        json_file_path = os.path.dirname(__file__) + "/../data/" + output_file_name
 
-        with open(json_filename, 'w') as file:
+        with open(json_file_path, 'w') as file:
             json.dump([status._json for status in all_tweets], file)
 
 
 def main():
     grabber = TweepyGrabber()
     api = grabber.api_connect(os.environ['TWEET_PUB'], os.environ['TWEET_PRI'])
-    grabber.get_users_timeline(api, "patrickbeekman")
+    grabber.get_users_timeline(api, "patrickbeekman", "patricks_tweets.json")
 
 if __name__ == "__main__":
     main()
