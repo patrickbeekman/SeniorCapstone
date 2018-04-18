5a3265bc7ea3b7adca1437c0c15d94ca9aeb0b8e
patrickbeekman <patrickbeekman1095@gmail.com>
2018-04-18 10:36:38 -0400

    Added more of my plots to the website.

--- src/flask_app.py
+++ src/flask_app.py
@@ -22,13 +22,16 @@ def index():
 
     name = request.args.get("name")
     if name == None:
-        name = "Edward"
+        name = "Patrick"
     return render_template("index.html", name=name,
                            tweet_freq_script=plot_components['time_series_script'], tweet_freq_div=plot_components['time_series_div'],
                            tweets_hourly_script=plot_components['tweets_hour_script'], tweets_hourly_div=plot_components['tweets_hour_div'],
                            hourly_emotion_script=plot_components['hourly_emotion_script'], hourly_emotion_div=plot_components['hourly_emotion_div'],
                            unNormalized_FavsRTs_script=plot_components['unNormalized_FavsRTs_script'], unNormalized_FavsRTs_div=plot_components['unNormalized_FavsRTs_div'],
-                           normalized_FavsRTs_script=plot_components['normalized_FavsRTs_script'], normalized_FavsRTs_div=plot_components['normalized_FavsRTs_div'])
+                           normalized_FavsRTs_script=plot_components['normalized_FavsRTs_script'], normalized_FavsRTs_div=plot_components['normalized_FavsRTs_div'],
+                           norm_FavsRTs_emotion_script=plot_components['norm_FavsRTs_emotion_script'], norm_FavsRTs_emotion_div=plot_components['norm_FavsRTs_emotion_div'],
+                           emotions_word_ct_script=plot_components['emotions_word_ct_script'], emotions_word_ct_div=plot_components['emotions_word_ct_div'],
+                           days_of_week_script=plot_components['days_of_week_script'], days_of_week_div=plot_components['days_of_week_div'])
 
 # With debug=True, Flask server will auto-reload
 # when there are code changes
